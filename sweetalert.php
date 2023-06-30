a066e789b1dd9805b2453f7a69a6d5a929687d51
maryguo <mary.guo@jmaconsulting.biz>
2023-06-30 14:47:56 -0600

    add second folders

---
+++ sweetalert.php
@@ -0,0 +1,146 @@
+<?php
+
+require_once 'sweetalert.civix.php';
+use CRM_Sweetalert_ExtensionUtil as E;
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function sweetalert_civicrm_config(&$config) {
+  _sweetalert_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function sweetalert_civicrm_xmlMenu(&$files) {
+  _sweetalert_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function sweetalert_civicrm_install() {
+  _sweetalert_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function sweetalert_civicrm_postInstall() {
+  _sweetalert_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function sweetalert_civicrm_uninstall() {
+  _sweetalert_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function sweetalert_civicrm_enable() {
+  _sweetalert_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function sweetalert_civicrm_disable() {
+  _sweetalert_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function sweetalert_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _sweetalert_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function sweetalert_civicrm_managed(&$entities) {
+  _sweetalert_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
+function sweetalert_civicrm_caseTypes(&$caseTypes) {
+  _sweetalert_civix_civicrm_caseTypes($caseTypes);
+}
+
+/**
+ * Implements hook_civicrm_angularModules().
+ *
+ * Generate a list of Angular modules.
+ *
+ * Note: This hook only runs in CiviCRM 4.5+. It may
+ * use features only available in v4.6+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+ */
+function sweetalert_civicrm_angularModules(&$angularModules) {
+  _sweetalert_civix_civicrm_angularModules($angularModules);
+}
+
+/**
+ * Implements hook_civicrm_alterSettingsFolders().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
+ */
+function sweetalert_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
+  _sweetalert_civix_civicrm_alterSettingsFolders($metaDataFolders);
+}
+
+/**
+ * Implements hook_civicrm_entityTypes().
+ *
+ * Declare entity types provided by this module.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
+ */
+function sweetalert_civicrm_entityTypes(&$entityTypes) {
+  _sweetalert_civix_civicrm_entityTypes($entityTypes);
+}
+
+/**
+ * Implements hook_civicrm_coreResourceList().
+ */
+function sweetalert_civicrm_coreResourceList(&$items, $region) {
+  if ($region === 'html-header') {
+    Civi::resources()
+      ->addStyleFile(E::SHORT_NAME, 'css/sweetalert2.min.css', 0, $region)
+      ->addScriptFile(E::SHORT_NAME, 'js/sweetalert2.min.js', 0, $region);
+  }
+}
